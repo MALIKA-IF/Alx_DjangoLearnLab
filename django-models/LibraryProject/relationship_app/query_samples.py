075226e841289c26a35416cfaf2ef61ecfafa72d
Malika Edaif <edmalika957@gmail.com>
2025-02-23 16:40:37 +0100

    create database

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,13 @@
 from .models import Author, Book, Librarian, Library
 
-author = Author.objects.get(name="Author_name")
-
-books_author = author.books.all()
+author=Author.objects.get(name="Author_name")
+books_author=author.books.all()
    
-library = Library.objects.get(name="library_name")
+library=Library.objects.get(name="library_name")
 books_library = library.books.all()   
 
-book = Book.objects.get(title="Book_title")
 
-library = book.library
-librarian = library.librarian
+book=Book.objects.get(title="Book_title")
+library=book.library
+librarian=library.librarian
 print(librarian.name)    
