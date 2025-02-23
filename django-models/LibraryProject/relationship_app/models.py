e5b56160413d10b7634eacc45e1d43720454eead
Malika Edaif <edmalika957@gmail.com>
2025-02-23 15:18:32 +0100

    create database

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 # Create your models here.
-user = get_user_model()
 
-class Author (models.Model):
+class Author(models.Model):
     name=models.CharField(max_length=20)
 
 def __str__(self):
@@ -12,7 +10,7 @@ def __str__(self):
 
 class Book(models.Model):
     title=models.CharField(max_length=100)
-    
+
 def __str__(self):
     return self.title
    
