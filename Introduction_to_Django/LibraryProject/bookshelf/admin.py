b0dac4642e72920ea956d1041366025c809928f4
Malika Edaif <edmalika957@gmail.com>
2025-02-23 14:09:01 +0100

    create databases

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,7 +5,7 @@ from .models import Book
 
 admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_display = ('title', 'author', 'published_year')
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
