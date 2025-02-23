e9b6d42904c69289781b5d34aa798644b15b146e
Malika Edaif <edmalika957@gmail.com>
2025-02-23 14:20:48 +0100

    create database

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Author (models.Model):
+class Author(models.Model):
     name=models.CharField(max_length=20)
+    
 
 class Book(models.Model):
     title=models.CharField(max_length=100)
