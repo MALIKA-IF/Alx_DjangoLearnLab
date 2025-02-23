136aea0c1fed0ac94c19a1f583fad0bc07fde6f6
Malika Edaif <edmalika957@gmail.com>
2025-02-23 15:17:22 +0100

    create database

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+user = get_user_model()
 
-class Author(models.Model):
+class Author (models.Model):
     name=models.CharField(max_length=20)
-    
+
+def __str__(self):
+    return self.name
 
 class Book(models.Model):
     title=models.CharField(max_length=100)
+    
+def __str__(self):
+    return self.title
+   
     author=models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  
 class Library(models.Model):
     name=models.CharField(max_length=100)
