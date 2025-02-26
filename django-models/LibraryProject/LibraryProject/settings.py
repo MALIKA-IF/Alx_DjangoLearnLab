7fde29d3b2693dea2a8b73d321a1c2c62d39910c
Malika Edaif <edmalika957@gmail.com>
2025-02-26 23:23:27 +0100

    modify file setting.py

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'relationship_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from .views import list, list_book
 
 urlpatterns = [
     path('admin/', admin.site.urls),
