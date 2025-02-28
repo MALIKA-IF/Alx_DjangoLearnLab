a5968ff994698d6da4dcaa6ee25f66669ffd6721
Malika Edaif <edmalika957@gmail.com>
2025-03-01 00:52:15 +0100

    modify file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from.models import Book
 
 # Create your views here.
-
+Book.objects.all()
 
 def index(request):
+
     return render(request,'relationship_app/list_books.html')
 
