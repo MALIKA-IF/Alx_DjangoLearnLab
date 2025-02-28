1a387b8b7f080a6adbf53271b90e777e7d17d22a
Malika Edaif <edmalika957@gmail.com>
2025-03-01 00:42:30 +0100

    modify file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("", views.index2, name="index2"),
    
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from.models import Book
 
 # Create your views here.
-#def list_book(request):
-#   books=Book.objects.all()
-#   return render(f"the title is {books.title} create by {books.author}")
+
 
 def index(request):
     return render(request,'relationship_app/list_books.html')
+
+def index2(request):
+    return render(request,'relationship_app/library_detail.html')
