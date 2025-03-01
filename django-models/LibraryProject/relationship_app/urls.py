29f267193969bf82ccc8eefd7bfc757fcb5854a8
Malika Edaif <edmalika957@gmail.com>
2025-03-01 20:38:02 +0100

    modify file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView,RegisterView,Login,Logout
+from .views import list_books, LibraryDetailView,Register,Login,Logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path("", list_books, name="index"),
     path("",LibraryDetailView.as_view(),name="details"),
-    path("Login/", Login.as_view(), name='login'),
-    path("RegisterView/", RegisterView.as_view(), name='register'),
-    path("Logout/", Logout.as_view(), name='logout'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("register/", views.Register, name='register'),
+    path("logout/", LogoutView.as_view(), name='logout'),
 
 
-
+#views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
+
 from django.contrib.auth import login
 
 
@@ -22,20 +21,10 @@ class LibraryDetailView(DetailView):
 
     template_name = "relationship_app/library_detail.html"
 
-class RegisterView(CreateView):
+class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "relationship_app/register.html"
 
-class Login(LoginView):
 
-    template_name="relationship_app/login.html"
-
-class Logout(LogoutView):
-
-    template_name="relationship_app/logout.html"
-
-    
-
-    
     
