48a13bd732facb54f4532bad705fb6200998706e
Malika Edaif <edmalika957@gmail.com>
2025-03-02 22:41:31 +0100

    create new file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 
-
+permission_required('app_name.can_edit', raise_exception=True)
 AUTH_USER_MODEL = "relationship_app.CustomUser()"
