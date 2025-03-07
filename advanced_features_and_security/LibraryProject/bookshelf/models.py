33bfcd3c3fdb78cb3c55e34c48606a312b1f5769
Malika Edaif <edmalika957@gmail.com>
2025-03-07 18:09:33 +0100

    modify file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
 
 
 
@@ -28,6 +30,27 @@ class CustomUserManager(BaseUserManager):
         pass
     def create_superuser(self,email="None", password="None"):
         pass
+
+
+admin_group =Group.objects.create(name='Admins')
+editor_group = Group.objects.create(name='Editors')
+viewer_group=Group.objects.create(name='Viewers')
+
+   
+view_permission = Permission.objects.get(codename='can_view')
+create_permission = Permission.objects.get(codename='can_create')
+edit_permission = Permission.objects.get(codename='can_edit')
+delete_permission = Permission.objects.get(codename='can_delete')
+
+admin_group.permissions.add(view_permission)
+admin_group.permissions.add(create_permission)
+admin_group.permissions.add(edit_permission)
+admin_group.permissions.add(delete_permission)
+
+editor_group.permissions.add(editor_group)
+editor_group.permissions.add(create_permission)
+
+viewer_group.permissions.add(view_permission)
 
 
 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Permission,User,Group
+from .models import Admin,Viewer,Editor
+from django.http import HttpResponse
 
 # Create your views here.
 
-permission_required('app_name.can_edit', raise_exception=True)
-AUTH_USER_MODEL = "relationship_app.CustomUser()"
+#AUTH_USER_MODEL = "relationship_app.CustomUser()"
+permission_required('bookshelf.can_edit', raise_exception=True)
+def add_book(request):
+    return HttpResponse("book added")
+permission_required('bookshelf.can_view', raise_exception=True)
+def add_book(request):
+    return HttpResponse("book viewed")
+permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    return HttpResponse("book created")
+permission_required('bookshelf.can_delete', raise_exception=True)
+def add_book(request):
+    return HttpResponse("book deleted")
+
+
+
