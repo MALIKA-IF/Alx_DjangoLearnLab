3cc1fc34c602d9d1ddc5ad75afa90c3171622f0c
Malika Edaif <edmalika957@gmail.com>
2025-03-23 13:15:30 +0100

    create urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('/post/', PostListView.as_view(template_name='Post_list.html'), name="PostList"),
     path('/post/new/', PostCreateView.as_view(template_name='create_Post.html'), name="NewPost"),
     path('/post/<int:pk>/', PostDetailView.as_view(template_name='Post_detail.html'), name="DetailPost"),
-    path('/post/<int:pk>/edit/', PostUpdateView.as_view(template_name='update_Post.html'), name="UpdatPost"),
+    path('/post/<int:pk>/update/', PostUpdateView.as_view(template_name='update_Post.html'), name="UpdatPost"),
     path('/post/<int:pk>/delete/', PostDeleteView.as_view(template_name='delete_Post.html'), name="DeletePost"),
     
 
