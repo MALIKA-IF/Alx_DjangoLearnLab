cf97b45e5b2fa2e8c9929d0b30731205b3eb1474
Malika Edaif <edmalika957@gmail.com>
2025-03-23 13:13:59 +0100

    create urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', RegistrationView.as_view(template_name='register.html'), name='register'),
     path('profile/', profile.as_view(template_name='profile.html'), name='profile'),
-    path('/posts/', PostListView.as_view(template_name='Post_list.html'), name="PostList"),
-    path('/posts/new/', PostCreateView.as_view(template_name='create_Post.html'), name="NewPost"),
-    path('/posts/<int:pk>/', PostDetailView.as_view(template_name='Post_detail.html'), name="DetailPost"),
-    path('/posts/<int:pk>/edit/', PostUpdateView.as_view(template_name='update_Post.html'), name="UpdatPost"),
-    path('/posts/<int:pk>/delete/', PostDeleteView.as_view(template_name='delete_Post.html'), name="DeletePost"),
+    path('/post/', PostListView.as_view(template_name='Post_list.html'), name="PostList"),
+    path('/post/new/', PostCreateView.as_view(template_name='create_Post.html'), name="NewPost"),
+    path('/post/<int:pk>/', PostDetailView.as_view(template_name='Post_detail.html'), name="DetailPost"),
+    path('/post/<int:pk>/edit/', PostUpdateView.as_view(template_name='update_Post.html'), name="UpdatPost"),
+    path('/post/<int:pk>/delete/', PostDeleteView.as_view(template_name='delete_Post.html'), name="DeletePost"),
     
 
 
