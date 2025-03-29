2e5de4678834c750b3050203c7d46f80f643566c
Malika Edaif <edmalika957@gmail.com>
2025-03-30 00:39:10 +0100

    add urls

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post,Comment
+from .serializers import Postserializer,Commentserializer
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = Commentserializer  
