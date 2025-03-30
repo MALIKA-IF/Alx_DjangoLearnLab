c3c71b0489e31058dff89554283be89a21edc2ca
Malika Edaif <edmalika957@gmail.com>
2025-03-30 03:10:31 +0200

    add urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import register,Login
+from .views import register,Login,FollowApiView
 
 urlpatterns = [
    
     path('register/',register.as_view(), name='register'),
     path('login/',Login, name='login'),
+    path('follow/<int:user_id>/',FollowApiView, name='Follow'),
+    path('unfollow/<int:user_id>/',FollowApiView, name='unFollow'),
+    
+
+
 ]
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import FeedApiView
 
 urlpatterns = [
+    path('/feed/',FeedApiView, name='feed'),
   
     
 
