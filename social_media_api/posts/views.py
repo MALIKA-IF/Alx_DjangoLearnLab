866ffa6d1bbda00cf66b50038d916ac5fc0b7f7f
Malika Edaif <edmalika957@gmail.com>
2025-03-30 01:49:17 +0100

    create class FeedApiView

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets,generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets,generics,permissions
 from .models import Post,Comment
 from .serializers import Postserializer,Commentserializer
+
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
@@ -12,5 +12,14 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = Commentserializer  
+
+class FeedApiView(generics.GenericAPIView):
+    serializer_class = Postserializer
+    permission_classes=permissions.IsAuthenticated
+
+    def get(self):
+        following_users=Post.objects.filter(author__in=following_users).order_by('-created_at')
+        return following_users.all()
+
 
 
